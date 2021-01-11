52cdf9a7be6212f299dc0d8e661de934a4367c57
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2021-01-11 17:22:37 -0300

    avoid broken link

--- s/download_video.sh
+++ s/download_video.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-wget -c -O v/bunny_1080p_60fps.mp4 http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4
-wget -c -O v/bunny_1080p_30fps.mp4 http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4
+
+# the links aren't work anymore :(
+# wget -c -O v/bunny_1080p_60fps.mp4 http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4
+# wget -c -O v/bunny_1080p_30fps.mp4 http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4
+
+echo "we already provide the smaller versions at https://github.com/leandromoreira/digital_video_introduction/tree/master/v"
