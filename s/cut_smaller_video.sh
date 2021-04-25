3069b9bfaa7b6f449f3298758f7df5891b0118ce
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2021-04-25 07:40:15 -0300

    fix #111

--- s/cut_smaller_video.sh
+++ s/cut_smaller_video.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-./s/ffmpeg -y -i /files/v/bunny_1080p_60fps.mp4 -ss 00:01:24 -t 00:00:10  /files/v/small_bunny_1080p_60fps.mp4
-./s/ffmpeg -y -i /files/v/bunny_1080p_30fps.mp4 -ss 00:01:24 -t 00:00:10  /files/v/small_bunny_1080p_30fps.mp4
+
+#./s/ffmpeg -y -i /files/v/bunny_1080p_60fps.mp4 -ss 00:01:24 -t 00:00:10  /files/v/small_bunny_1080p_60fps.mp4
+#./s/ffmpeg -y -i /files/v/bunny_1080p_30fps.mp4 -ss 00:01:24 -t 00:00:10  /files/v/small_bunny_1080p_30fps.mp4
+
+echo "These files are already downloaded"
