e27dd4314e497c55cd817e449b801089ea3bc309
Soroush Sherafat <71870584+sorousherafat@users.noreply.github.com>
2023-02-24 12:09:30 +0330

    add path escaping to `start_jupyter.sh`
    with this proposed change, `$(pwd)` is escaped and users won't receive errors if the working directory path includes space for example.

--- s/start_jupyter.sh
+++ s/start_jupyter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Run Jupyter Notebook (Python 3.5.2, scipy 0.17.1)
-docker run --rm -p 8888:8888 -v $(pwd):/home/jovyan/work  jupyter/scipy-notebook:387f29b6ca83
+docker run --rm -p 8888:8888 -v "$(pwd)":/home/jovyan/work  jupyter/scipy-notebook:387f29b6ca83
